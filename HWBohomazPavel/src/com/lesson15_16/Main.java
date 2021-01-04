19ab6b9e28a3815daad12bc2f0713699f4a46ef2
Bohomaz Pavel <bogi1598@gmail.com>
2021-01-04 12:28:08 +0200

    Corrected HW#16

--- HWBohomazPavel/src/com/lesson15_16/Main.java
+++ HWBohomazPavel/src/com/lesson15_16/Main.java
@@ -8,10 +8,14 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
-        int num = scanner.nextInt();
-        char[] numbers = String.valueOf(num).toCharArray();
+        String num = scanner.nextLine();
+        char[] numbers = num.toCharArray();
+        drawNumber(numbers);
 
-        String[][] drawNumber = {{""}, {""}, {""}, {""}, {""}, {""}, {""}, {""},{""}};
+    }
+
+    public static void drawNumber(char[] numbers) {
+        String[][] drawNumber = {{""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}};
         for (int i = 0; i < numbers.length; i++) {
             String[][] current = choose(numbers[i]);
             for (int j = 0; j < current.length; j++) {
@@ -27,6 +31,20 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    private static String[][] drawNumber(char o) {
+        char[] numbers = String.valueOf(Integer.valueOf(o)).toCharArray();
+        String[][] drawNumber = {{""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}};
+        for (int i = 0; i < numbers.length; i++) {
+            String[][] current = choose(numbers[i]);
+            for (int j = 0; j < current.length; j++) {
+                for (int k = 0; k < current[i].length; k++) {
+                    drawNumber[j][k] += current[j][k];
+                }
+            }
+        }
+        return drawNumber;
     }
 
     private static String[][] choose(char o) {
@@ -51,153 +69,200 @@ public class Main {
                 return drawEight();
             case '9':
                 return drawNine();
+            default:
+//                String[][] skip = {{""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}};
+                return drawNumber(o);
         }
-        return null;
     }
 
     private static String[][] drawZero() {
         String[][] zero = {
-                {" ❄❄❄❄❄❄  "},
-                {"❄        ❄ "},
-                {"❄ ❄❄❄❄ ❄ "},
-                {"❄ ❄   ❄ ❄ "},
-                {"❄ ❄   ❄ ❄ "},
-                {"❄ ❄   ❄ ❄ "},
-                {"❄ ❄❄❄❄ ❄ "},
-                {"❄        ❄ "},
-                {" ❄❄❄❄❄❄  "}};
+                {"        @@@@@@@          "},
+                {"     @@@       @@@       "},
+                {"   @@@           @@@     "},
+                {"  @@               @@    "},
+                {"@@       @@@@@       @@  "},
+                {"@@     @@     @@     @@  "},
+                {"@@    @@       @@    @@  "},
+                {"@@     @@     @@     @@  "},
+                {"@@       @@@@@       @@  "},
+                {"  @@               @@    "},
+                {"   @@@           @@@     "},
+                {"     @@@       @@@       "},
+                {"        @@@@@@@          "}
+        };
         return zero;
     }
+
     private static String[][] drawOne() {
         String el = "\uD83C\uDF84"; // для удобства
         String[][] one = {
-                {"ᅠ   " + el + el + " "},
-                {"   "+el+el+el+" "},
-                {"  "+el+" "+el+el+" "},
-                {" "+el+"  "+el+el+" "},
-                {el+"   "+el+el+" "},
-                {"ᅠ   "+el+el+" "},
-                {"ᅠ   "+el+el+" "},
-                {"ᅠ   "+el+el+" "},
-                {"ᅠ   "+el+el+" "}
+                {"      @@@@@        "},
+                {"     @@  @@        "},
+                {"    @@   @@        "},
+                {"   @@    @@        "},
+                {"  @@     @@        "},
+                {" @@      @@        "},
+                {"@@       @@        "},
+                {"         @@        "},
+                {"         @@        "},
+                {"         @@        "},
+                {"         @@        "},
+                {"       @@@@@@      "},
+                {"   @@@@@@@@@@@@@@  "}
         };
         return one;
     }
+
     private static String[][] drawTwo() {
         String[][] two = {
-                {"ᅠ❄❄❄❄ "},
-                {"❄ᅠ   ❄ "},
-                {"❄ᅠ   ❄ "},
-                {"ᅠᅠ   ❄ "},
-                {"ᅠᅠ  ❄  "},
-                {"ᅠᅠ❄    "},
-                {"ᅠ❄   ᅠ "},
-                {"❄ᅠ   ᅠ "},
-                {"❄❄❄❄❄ "}
+                {"    @@@@@@@@@@     "},
+                {"   @@        @@    "},
+                {" @@          @@    "},
+                {"@@            @@   "},
+                {"            @@     "},
+                {"           @@      "},
+                {"         @@        "},
+                {"        @@         "},
+                {"      @@           "},
+                {"     @@            "},
+                {"   @@              "},
+                {" @@@               "},
+                {"@@@@@@@@@@@@@@@@@  "}
         };
         return two;
     }
+
     private static String[][] drawThree() {
-        String el = "\uD83C\uDF84";
         String[][] three = {
-                {el+el+el+el+"ᅠᅠ "},
-                {"ᅠᅠᅠᅠ"+el+"ᅠ "},
-                {"ᅠᅠᅠᅠᅠ"+el+" "},
-                {"ᅠᅠᅠᅠ"+el+"ᅠ "},
-                {"ᅠ"+el+el+el+"ᅠᅠ "},
-                {"ᅠᅠᅠᅠ"+el+"ᅠ "},
-                {"ᅠᅠᅠᅠᅠ"+el+" "},
-                {"ᅠᅠᅠᅠ"+el+"ᅠ "},
-                {el+el+el+el+"ᅠᅠ "}
+                {"@@@@@@@@@@@@      "},
+                {"            @@    "},
+                {"             @@   "},
+                {"              @@  "},
+                {"             @@   "},
+                {"      @@@@@@@@    "},
+                {"    @@@@@@@@      "},
+                {"      @@@@@@@@    "},
+                {"             @@   "},
+                {"              @@  "},
+                {"             @@   "},
+                {"            @@    "},
+                {"@@@@@@@@@@@@      "}
         };
         return three;
     }
+
     private static String[][] drawFour() {
         String[][] four = {
-                {"ᅠᅠᅠᅠ ❄ "},
-                {"ᅠᅠᅠ❄ ❄ "},
-                {"ᅠᅠ❄ ᅠ❄ "},
-                {"ᅠ❄ᅠ ᅠ❄ "},
-                {"❄❄❄❄❄❄ "},
-                {"ᅠᅠᅠᅠ ❄ "},
-                {"ᅠᅠᅠᅠ ❄ "},
-                {"ᅠᅠᅠᅠ ❄ "},
-                {"ᅠᅠᅠᅠ ❄ "}
+                {"         @@   @@  "},
+                {"        @@    @@  "},
+                {"      @@      @@  "},
+                {"     @@       @@  "},
+                {"   @@         @@  "},
+                {"  @@          @@  "},
+                {"@@            @@  "},
+                {"@@@@@@@@@@@@@@@@  "},
+                {"              @@  "},
+                {"              @@  "},
+                {"              @@  "},
+                {"              @@  "},
+                {"              @@  "}
         };
         return four;
     }
+
     private static String[][] drawFive() {
-        String el = "\uD83C\uDF84";
         String[][] five = {
-                {el+el+el+el+el+el+" "},
-                {el+"ᅠᅠᅠᅠᅠ "},
-                {el+el+el+el+el+"ᅠ "},
-                {"ᅠᅠᅠᅠᅠ"+el+" "},
-                {"ᅠᅠᅠᅠᅠ"+el+" "},
-                {"ᅠᅠᅠᅠᅠ"+el+" "},
-                {"ᅠᅠᅠᅠᅠ"+el+" "},
-                {"ᅠᅠᅠᅠᅠ"+el+" "},
-                {el+el+el+el+el+"ᅠ "},
+                {"@@@@@@@@@@@@@@@@@@@@  "},
+                {"@@                    "},
+                {"@@                    "},
+                {"@@@@@@@@@@@@          "},
+                {"           @@@        "},
+                {"              @@      "},
+                {"               @@     "},
+                {"                 @@   "},
+                {"                  @@  "},
+                {"                 @@   "},
+                {"               @@     "},
+                {"            @@@       "},
+                {"@@@@@@@@@@@@@         "}
 
         };
         return five;
     }
+
     private static String[][] drawSix() {
-        String el = "\uD83C\uDF84";
         String[][] six = {
-                {"ᅠ"+el+el+el+el+el+" "},
-                {el+"ᅠᅠᅠᅠᅠ "},
-                {el+"ᅠᅠᅠᅠᅠ "},
-                {el+el+el+el+el+el+" "},
-                {el+"ᅠᅠᅠᅠ"+el+" "},
-                {el+"ᅠ"+el+el+"ᅠ"+el+" "},
-                {el+"ᅠ"+el+el+"ᅠ"+el+" "},
-                {el+"ᅠ"+"ᅠᅠᅠ"+el+" "},
-                {el+el+el+el+el+el+" "}
+                {"         @@        "},
+                {"       @@          "},
+                {"      @@           "},
+                {"    @@             "},
+                {"   @@              "},
+                {" @@@@@@@@@@@@@     "},
+                {"@@           @@    "},
+                {"@@             @@  "},
+                {"@@             @@  "},
+                {"@@             @@  "},
+                {"  @@         @@    "},
+                {"   @@       @@     "},
+                {"     @@@@@@@       "}
         };
         return six;
     }
+
     private static String[][] drawSeven() {
-        String el = "\uD83C\uDF84";
         String[][] seven = {
-                {"ᅠ❄❄❄❄❄❄❄ "},
-                {" ᅠ       ❄  "},
-                {" ᅠ     ❄    "},
-                {" ᅠ   ❄      "},
-                {" ᅠ   ❄      "},
-                {" ᅠ   ❄      "},
-                {" ᅠ   ❄      "},
-                {" ᅠ   ❄      "},
-                {" ᅠ   ❄      "},
+                {"@@@@@@@@@@@@@@@@@  "},
+                {"              @@   "},
+                {"             @@    "},
+                {"            @@     "},
+                {"           @@      "},
+                {"          @@       "},
+                {"        @@         "},
+                {"       @@          "},
+                {"       @@          "},
+                {"       @@          "},
+                {"       @@          "},
+                {"       @@          "},
+                {"       @@          "}
         };
         return seven;
     }
+
     private static String[][] drawEight() {
         String[][] eight = {
-                {"ᅠ❄❄❄❄❄❄ᅠ "},
-                {"❄ᅠ      ᅠ❄ "},
-                {"❄ᅠ      ᅠ❄ "},
-                {"❄ᅠ      ᅠ❄ "},
-                {"ᅠ❄❄❄❄❄❄ᅠ "},
-                {"❄ᅠ      ᅠ❄ "},
-                {"❄ᅠ      ᅠ❄ "},
-                {"❄ᅠ      ᅠ❄ "},
-                {"ᅠ❄❄❄❄❄❄ᅠ "}
+                {"    @@@@@@@@@@@@@      "},
+                {"  @@             @@    "},
+                {"@@       @@@       @@  "},
+                {"@@      @   @      @@  "},
+                {"  @@     @@@     @@    "},
+                {"   @@           @@     "},
+                {"     @@@@@@@@@@@       "},
+                {"   @@           @@     "},
+                {"  @@     @@@     @@    "},
+                {"@@      @   @      @@  "},
+                {"@@       @@@       @@  "},
+                {"  @@             @@    "},
+                {"    @@@@@@@@@@@@@      "}
         };
         return eight;
     }
+
     private static String[][] drawNine() {
-        String el = "\uD83C\uDF84";
         String[][] nine = {
-                {"ᅠ"+el+el+el+el+"ᅠ "},
-                {el+"ᅠᅠᅠᅠ"+el+" "},
-                {el+"ᅠ"+el+el+"ᅠ"+el+" "},
-                {el+"ᅠ"+el+el+"ᅠ"+el+" "},
-                {el+"ᅠ"+"ᅠᅠᅠ"+el+" "},
-                {"ᅠ"+el+el+el+el+el+" "},
-                {"ᅠᅠᅠᅠᅠ"+el+" "},
-                {"ᅠᅠᅠᅠᅠ"+el+" "},
-                {el+el+el+el+el+"ᅠ "}
+                {"      @@@@@@@@@        "},
+                {"    @@         @@      "},
+                {"  @@     @@@     @@    "},
+                {"@@     @@   @@     @@  "},
+                {"@@     @@   @@     @@  "},
+                {"  @@     @@@     @@    "},
+                {"    @@         @@      "},
+                {"      @@@@@@@@@        "},
+                {"            @@         "},
+                {"           @@          "},
+                {"          @@           "},
+                {"         @@            "},
+                {"        @@             "}
         };
         return nine;
     }
