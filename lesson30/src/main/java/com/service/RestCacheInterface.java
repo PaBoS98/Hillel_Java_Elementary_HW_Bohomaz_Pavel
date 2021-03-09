4e3ae8295d9c2a4c993a96445e8d80a084dd3c15
Bohomaz Pavel <Bogi1598@gmail.com>
2021-03-09 13:50:36 +0200

    correct lesson 30

--- lesson30/src/main/java/com/service/RestCacheInterface.java
+++ lesson30/src/main/java/com/service/RestCacheInterface.java
@@ -1,5 +1,11 @@
 package com.service;
 
+/**
+ * @author Bohomaz Pavel
+ * @version 0.0.1
+ * <p>Interface for creating a cache using REST</p
+ * <p>A class that implements this interface must also extend the Cache class and use its methods to manage changes in the cache.</p>
+ */
 public interface RestCacheInterface {
 
     /**
